52ca20497544f79e27434de27bdcffa2f826d8b7
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-19 01:55:26 -0500

    WIP p 31

---
+++ 02/hello-js-world/js/script.js
@@ -0,0 +1,12 @@
+/* With a variable */
+/*const h1 = React.createElement('h1', null, 'Hello World!');
+ReactDOM.render(
+  h1,
+  document.getElementById('content')
+)*/
+
+/* Without a Variable */
+ReactDOM.render(
+  React.createElement('hw', null, 'Hello World!'),
+  document.getElementById('content')
+);
