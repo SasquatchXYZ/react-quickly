248b6c39c4f43db6398b5a980ace276a3dd2f271
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-22 13:15:19 -0500

    Completed Chapter 4

--- 04/clock-analog-digital/jsx/analog-display.jsx
+++ 04/clock-analog-digital/jsx/analog-display.jsx
@@ -1,9 +1,48 @@
 const AnalogDisplay = (props) => {
   const date = new Date(props.time);
-  const dialStyle = {};
-  const secondHandStyle = {};
-  const minuteHandStyle = {};
-  const hourHandStyle = {};
+  const dialStyle = {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    width: 200,
+    height: 200,
+    borderRadius: 20000,
+    borderStyle: 'solid',
+    borderColor: 'black'
+  };
+  const secondHandStyle = {
+    position: 'relative',
+    top: 100,
+    left: 100,
+    border: '1px solid red',
+    width: '40%',
+    height: 1,
+    transform: 'rotate(' + ((date.getSeconds()/60)*360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'red'
+  };
+  const minuteHandStyle = {
+    position: 'relative',
+    top: 100,
+    left: 100,
+    border: '1px solid grey',
+    width: '40%',
+    height: 3,
+    transform: 'rotate(' + ((date.getMinutes()/60)*360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'grey'
+  };
+  const hourHandStyle = {
+    position: 'relative',
+    top: 92,
+    left: 106,
+    border: '1px solid grey',
+    width: '20%',
+    height: 7,
+    transform: 'rotate(' + ((date.getHours()/12)*360 - 90).toString() + 'deg)',
+    transformOrigin: '0% 0%',
+    backgroundColor: 'grey'
+  };
 
   return (
     <div>
