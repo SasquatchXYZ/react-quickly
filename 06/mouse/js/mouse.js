b39006e21b2186dd393f3a7d5582fab30e0cfe0e
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-25 21:17:21 -0500

    Working through chapter 6

---
+++ 06/mouse/js/mouse.js
@@ -0,0 +1,69 @@
+// Nullifying a Synthetic Event
+class Mouse extends React.Component {
+  handleMouseOver(event) {
+    console.log('mouse is over with event');
+    window.e = event;
+    console.dir(event.target);
+    setTimeout(() => {
+      console.table(event.target);
+      console.table(window.e.target);
+    }, 2345);
+  }
+
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'div',
+        {
+          style: { border: '1px solid red' },
+          onMouseOver: this.handleMouseOver.bind(this)
+        },
+        'Open DevTools and move your mouse cursor over here'
+      )
+    );
+  }
+}
+
+// Event Handler as a Class Method; binding in render()
+/*class Mouse extends React.Component {
+  handleMouseOver (event) {
+    console.log('mouse is over with event');
+    console.dir(event.target)
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{border: '1px solid red'}}
+          onMouseOver={this.handleMouseOver.bind(this)}
+        >
+          Open DevTools and move your mouse cursor over here.
+        </div>
+      </div>
+    )
+  }
+}*/
+
+// Event Handler Receiving a Synthetic Event
+/*
+class Mouse extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{border: '1px solid red'}}
+          onMouseOver={(event => {
+            console.log('mouse is over with event');
+            console.dir(event)
+          })}
+        >
+          Open DevTools and Move Your Mouse Cursor Over Here.
+        </div>
+      </div>
+    )
+  }
+}*/
