aa7fadb5a51f9f2dcfe37a8fa2558e041b90074e
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-28 20:34:31 -0500

    WIP chapter 7

---
+++ 07/account/js/content.js
@@ -0,0 +1,34 @@
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { accountNumber: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log('Typed:', event.target.value);
+    this.setState({ accountNumber: event.target.value.replace(/[^0-9]/ig, '') });
+  }
+
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Account Number:',
+      React.createElement('input', {
+        type: 'text',
+        onChange: this.handleChange,
+        placeholder: '12345',
+        value: this.state.accountNumber
+      }),
+      React.createElement('br', null),
+      React.createElement(
+        'span',
+        null,
+        this.state.accountNumber.length > 0 ? `You entered: ${this.state.accountNumber}` : ''
+      )
+    );
+  }
+}
