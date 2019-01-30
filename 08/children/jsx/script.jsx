3a1910a9b10d6039ce14168f71d2061005f27606
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-30 00:18:49 -0500

    Chapter 08

--- 08/children/jsx/script.jsx
+++ 08/children/jsx/script.jsx
@@ -1,4 +1,18 @@
 ReactDOM.render(
-  <Content/>,
+  <div>
+    <Content>
+      <h1>React</h1>
+      <p>Rocks</p>
+    </Content>
+    <Content>
+      <img src="img/cube.png" width="200"/>
+    </Content>
+    <Content>
+      <a href="http://react.rocks">http://react.rocks</a>
+    </Content>
+    <Content>
+      <a className="btn btn-danger" href="http://react.rocks">http://react.rocks</a>
+    </Content>
+  </div>,
   document.getElementById('root')
 );
